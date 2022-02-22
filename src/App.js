f2b31cca6a9567ffe901dd6d61b3ae85a9ef1370
SVRourke <samuelvanrourke@gmail.com>
2022-02-22 00:04:40 -0500

    working on stlyes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState } from "react";
+import "./app.scss";
 
 function App() {
+  const [first, setFirst] = useState(5);
+  const [second, setSecond] = useState(15);
+
+  const runTimer = () => {
+    console.log("starting!");
+    setTimeout(() => {
+      console.log("Focus Done");
+      setTimeout(() => {
+        console.log("Meditation Done");
+      }, second * 60000);
+    }, first * 60000);
+  };
+  const inMillis = (minutes) => minutes;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-main">
+        <div className="display-row">
+          <Display number={first} cb={setFirst} subTitle={"warm up"} />
+          <div className="separator">:</div>
+          <Display number={second} cb={setSecond} subTitle={"warm up"} />
+        </div>
+        <button id="start" onClick={runTimer}>
+          Start
+        </button>
+      </div>
     </div>
   );
 }
+const Display = ({ number, cb, subTitle }) => {
+  const fmtD = (number) => (number < 10 ? `0${number}` : number);
+  return (
+    <div class="display">
+      <div className={"number"}>{fmtD(number)}</div>
+      <TimeControl stateRef={number} cb={cb} />
+    </div>
+  );
+};
+const TimeControl = ({ stateRef, cb }) => {
+  const clickHandler = (e) => {
+    cb(stateRef + parseInt(e.target.dataset.value));
+  };
+
+  return (
+    <div onClick={(e) => clickHandler(e)} className="control-buttons">
+      <div data-value="1">+</div>
+      <div data-value="-1">-</div>
+    </div>
+  );
+};
 
 export default App;
